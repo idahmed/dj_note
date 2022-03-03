cbf76db1d8d704fdbb7539beb317b0260361e5cb
idahmed <idahmedyassine@gmail.com>
2022-03-03 02:49:31 +0100

    :sparkles: add note app model stracture

---
+++ note_core/core/note/managers.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import logging
+
+from django.db import models
+
+logger = logging.getLogger(__name__)
+
+class NoteManager(models.Manager):
+    pass
---
+++ note_core/core/note/models.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+from pyexpat import model
+from django.utils.translation import gettext_lazy as _
+
+import typing
+
+from django.db import models
+
+from note_core.core.user.models import User
+
+class Note(models.Model):
+    """ Note model to represent notes in app.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    title = models.CharField(max_length=250, null=False, blank=False, help_text="Note title")
+    content = models.TextField(help_text="Note content")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name_plural = _("Notes")
+
+    @property
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> typing.Text:
+        return f"{self.email}"
+
+    def __unicode__(self) -> typing.Text:
+        return self.__str__()
