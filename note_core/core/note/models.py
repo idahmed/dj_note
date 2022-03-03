f58c5f5f8c6b7d4624850cedd343993cc226f450
idahmed <idahmedyassine@gmail.com>
2022-03-03 03:05:07 +0100

    add visibility

--- note_core/core/note/models.py
+++ note_core/core/note/models.py
@@ -12,11 +12,19 @@ class Note(models.Model):
     """ Note model to represent notes in app.
     """
 
+    class Visibility(models.TextChoices):
+        VISIBLE = "Visible", _("Visible")
+        HIDDEN = "Hidden", _("Hidden")
+        ARCHIVED = "Archived", _("Archived")
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     title = models.CharField(
         max_length=250, null=False, blank=False, help_text="Note title"
     )
     content = models.TextField(help_text="Note content")
+    visibility = models.CharField(
+        choices=Visibility.choices, max_length=100, default=Visibility.VISIBLE
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -24,12 +32,8 @@ class Note(models.Model):
     class Meta:
         verbose_name_plural = _("Notes")
 
-    @property
-    def get_full_name(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
     def __str__(self) -> typing.Text:
-        return f"{self.email}"
+        return f"{self.title}"
 
     def __unicode__(self) -> typing.Text:
         return self.__str__()
